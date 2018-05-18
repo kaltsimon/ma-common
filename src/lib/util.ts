447ecec879da2f696fd03ef4910fe34f8e96919d
Simon Kalt <kaltsimon@users.noreply.github.com>
2018-05-18 14:18:45 +0200

    export withStylesPure

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,12 @@
+import { withStyles } from '@material-ui/core/styles';
+import { compose, pure } from 'recompose';
+
 export const info = (tag: string) => (...values: any[]) => {
   if (process.env.NODE_ENV !== 'production') {
     console.info(`${tag}:`, ...values);
   }
+};
+
+export const withStylesPure: typeof withStyles = (style, options) => {
+  return compose(withStyles(style, options), pure);
 };
